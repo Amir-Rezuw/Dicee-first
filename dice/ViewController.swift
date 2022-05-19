a8973ef2182a5cbfd5348ff1fae7202b214f88df
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2022-05-19 11:50:15 +0430

    responsive design

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -9,20 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
         
     @IBOutlet weak var diceOne: UIImageView!
     
     @IBOutlet weak var diceTwo: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    
+    
+        
     @IBAction func rollButtonPress(_ sender: UIButton) {
         let rand1 = Int.random(in: 1...6);
         let rand2 = Int.random(in: 1...6);
-        let diceNames= ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+        let diceNames = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
         diceOne.image = UIImage(named: diceNames[rand1 - 1])
         diceTwo.image = UIImage(named:diceNames[rand2 - 1])
     }
